1e7c3b129e8db186f6db48900f4fede0f7df2639
AtakanSevincli <47077951+AtakanSevincli@users.noreply.github.com>
2021-03-19 20:15:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import bodyParser from "body-parser";
 import usersRoutes from "./routes/users.js";
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
@@ -15,6 +15,6 @@ app.all("*", (req, res) =>
   res.status(404).send("You've tried reaching a route that doesn't exist.")
 );
 
-app.listen(process.env.PORT || 5000, () =>
+app.listen(PORT, () =>
   console.log(`Server running on port: http://localhost:${PORT}`)
 );
